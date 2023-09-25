dba14af32f86478714fed1f10cd6f08c7780ddbd
carlafaes <carlafaes18@hotmail.com>
2023-09-24 21:18:30 -0300

    cambios de prueba

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import PaymentScreen from "./screens/PaymentScreen";
 import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 import OrderScreen from "./screens/OrderScreen";
 import NotFound from "./screens/NotFound";
+import NotFound from "./screens/NotFoundError";
 
 function App() {
   return (
