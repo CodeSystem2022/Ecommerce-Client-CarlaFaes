5447d1e23910db6a154282d0d9e04ff3778d562e
carlafaes <carlafaes18@hotmail.com>
2023-10-26 14:25:01 -0300

    se agregan botones en el componente Header para loggin y loggout

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,16 +5,43 @@ import { Link, useLocation } from "react-router-dom";
 const Header = () => {
   const cart = useSelector((state) => state.cart);
   const { cartItems } = cart;
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+  const logoutHandler= ()=>{
+    console.log("salio de la sesion")
+  }
 
-  return(
+  return (
     <>
-    <div>
-      Header
-      <Link to="/cart">
-        <i>icono de carrito</i>
-        <span>{cartItems.length}</span>
-      </Link>
-    </div>
+      <div>
+        Header
+        <div>
+          {userInfo ? (
+            <>
+            <p>Hola, {userInfo.name}</p>
+            <div>
+                <Link to="/profile">Perfil</Link>
+              </div>
+              <div>
+                <Link to="#" onClick={logoutHandler}>Cerrar sesion</Link>
+              </div>
+            </>
+          ) : (
+            <>
+              <div>
+                <Link to="/login">Iniciar sesion</Link>
+              </div>
+              <div>
+                <Link to="/register">Registrarse</Link>
+              </div>
+            </>
+          )}
+        </div>
+        <Link to="/cart">
+          <i>icono de carrito</i>
+          <span>{cartItems.length}</span>
+        </Link>
+      </div>
     </>
   );
 };
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,24 +1,67 @@
-import React from 'react'
-import { Link } from "react-router-dom"
-import Header from '../components/Header'
+import React, { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import Header from "../components/Header";
+import Message from "../components/LoadingError/Error.js"
+import Loading from "../components/LoadingError/Loading.js"
+import { login } from "../Redux/Actions/UserAction";
 
 const Login = () => {
-    window.scrollTo(0, 0);
+  window.scrollTo(0, 0);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  let history = useNavigate();
+  const location = useLocation();
+  const dispatch=useDispatch();
+  const redirect = location.search ? location.search.split("/")[1] : "/";
+
+  const userLogin= useSelector((state)=> state.userLogin);
+  const { error, loading, userInfo}= userLogin
+
+  useEffect(()=>{
+    if(userInfo){
+        //history.push(redirect)
+        history(redirect);
+    }
+  },[userInfo,history,redirect])
+
+  const submitHandler=(e)=>{
+    e.preventDefault()
+    dispatch(login(email,password))
+  }
   return (
     <div>
-        <Header/>
-        <div>
-            <form>
-                <input type='email' placeholder='Email'/>
-                <input type='password' placeholder='Password'/>
-                <button>Iniciar sesion</button>
-                <p>
-                    <Link to={"/register"}> Crear cuenta</Link>
-                </p>
-            </form>
-        </div>
+      <Header />
+      <div>
+        {error && <Message>{error}</Message>}
+        {loading && <Loading/>}
+        <form onSubmit={submitHandler}>
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit">Iniciar sesion</button>
+          <p>
+            <Link
+              to={redirect ? `/register?redirect=${redirect}` : "/register"}
+            >
+              {" "}
+              Crear cuenta
+            </Link>
+          </p>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
